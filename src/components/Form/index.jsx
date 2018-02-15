db7f26038c62de32b15654a2bf6aed64feb4c391
Abishek Aditya <abishekaditya@outlook.com>
2018-02-15 11:39:42 +0530

    Added class change for red color

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import './Form.css';
 
-const Form = () => (
-  <div className="form-container">
-    <input type="text" id="note-title-input" placeholder="Tasks for today" />
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.limit = 120;
+    this.state = {
+      isLimit: false,
+    };
+    this.note = '';
+  }
 
-    <p id="directions-text"><em>Please type your note below</em> &#128203;</p>
+  onTextChanged(e) {
+    this.note = e.target.value;
 
-    <textarea id="notes-area" />
-  </div>
-);
+    if (this.note.length >= this.limit) { this.note = this.note.slice(0, this.limit); }
+
+    this.setState({ isLimit: this.note.length >= 120 });
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <input type="text" id="note-title-input" placeholder="Tasks for today" />
+
+        <p id="directions-text"><em>Please type your note below</em> &#128203;</p>
+
+        <textarea
+          id="notes-area"
+          value={this.note}
+          className={this.state.isLimit ? 'text-limit-error' : ''}
+          onChange={e => this.onTextChanged(e)}
+        />
+      </div>
+    );
+  }
+}
+
 
 export default Form;
