fa2560a2af86560f5744d7c265e4291655af926c
Abishek Aditya <abishekaditya@outlook.com>
2018-02-16 11:17:39 +0530

    corrected uniqueId

--- src/components/HistoryItem/index.jsx
+++ src/components/HistoryItem/index.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import './HistoryItem.css';
 
-const HistoryItem = ({ text, titleText, uniqueId }) => (
-  <div className="HistoryItem-container" key={uniqueId}>
+const HistoryItem = ({ text, titleText }) => (
+  <div className="HistoryItem-container">
     <div className="HistoryItem-title">{titleText}</div>
     <div className="HistoryItem-text">{text}</div>
   </div>
 );
 
 HistoryItem.defaultProps = {
-  uniqueId: 0,
   text: 'Lorem ipsum deloret set ami',
   titleText: 'Default Title',
 };
 
 HistoryItem.propTypes = {
-  uniqueId: PropTypes.number,
   text: PropTypes.string,
   titleText: PropTypes.string,
 };
